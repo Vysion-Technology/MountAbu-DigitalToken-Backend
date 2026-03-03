18194ee76b55932d3175b30658de75fbcca96010
Avanish Gupta <avanish.sde@gmail.com>
2026-03-03 18:57:04 +0530

    fix: NULLABLE city profie data response API

--- backend/schemas/response/city_profile.py
+++ backend/schemas/response/city_profile.py
@@ -6,22 +6,22 @@ from pydantic import BaseModel, ConfigDict
 class CityProfileResponse(BaseModel):
     id: int
 
-    area_sq_km: Optional[str]
-    no_of_wards: Optional[int]
-    ward_boundaries: Optional[str]
-    population_estimate: Optional[int]
-    rental_properties_of_corporation: Optional[int]
-    number_of_slums: Optional[int]
-    solid_waste_per_day: Optional[str]
-    street_light_poles: Optional[int]
-    employees_in_board: Optional[int]
+    area_sq_km: Optional[str] = None
+    no_of_wards: Optional[int] = None
+    ward_boundaries: Optional[str] = None
+    population_estimate: Optional[int] = None
+    rental_properties_of_corporation: Optional[int] = None
+    number_of_slums: Optional[int] = None
+    solid_waste_per_day: Optional[str] = None
+    street_light_poles: Optional[int] = None
+    employees_in_board: Optional[int] = None
 
-    households_residential: Optional[int]
-    households_shops_offices: Optional[int]
-    households_open_plots: Optional[int]
+    households_residential: Optional[int] = None
+    households_shops_offices: Optional[int] = None
+    households_open_plots: Optional[int] = None
 
-    birth_registration_per_year: Optional[int]
-    birth_certificate_per_year: Optional[int]
+    birth_registration_per_year: Optional[int] = None
+    birth_certificate_per_year: Optional[int] = None
 
     created_by_id: int
     created_at: Optional[datetime] = None
